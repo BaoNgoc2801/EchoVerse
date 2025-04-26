f9a8a1ef19cdf7590b193fff7334a70791958a72
BaoNgoc2801 <baongoc2801np@gmail.com>
2025-04-26 18:55:50 +0700

    add webpack

--- micro-frontend-home/webpack.config.js
+++ micro-frontend-home/webpack.config.js
@@ -1,24 +1,15 @@
-// webpack.config.js của micro-frontend-home
-const path = require('path');
-const { ModuleFederationPlugin } = require('webpack').container;
+// webpack.config.js - micro-frontend-home
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        publicPath: 'auto',
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'home',
-            remotes: {
-                shell: 'shell@http://localhost:3000/remoteEntry.js',  // Địa chỉ của Shell App
+            name: "home",
+            filename: "remoteEntry.js",
+            exposes: {
+                './HomePage': './src/app/home/page.tsx',  // Expose HomePage
             },
+            shared: ["react", "react-dom"],
         }),
     ],
-    devServer: {
-        port: 3001,  // Port của micro-frontend home
-    },
 };
